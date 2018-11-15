0bdfddad6cbe146ac9cef342daa3cbcc092f7007
tekiy <tekiy@icloud.com>
2018-11-15 17:20:50 +0800

    添加Test.java

--- src/main/java/com/tekiy/eurekaserver/Test.java
+++ src/main/java/com/tekiy/eurekaserver/Test.java
@@ -4,7 +4,9 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Test");
-        
+
+        System.out.println("local");
+
         System.out.println("remote");
     }
 }
